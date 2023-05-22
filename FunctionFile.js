21c53a36bfbf11735cb06f6830ffc2cb10f21f22
Christopher Young <christopher.young.r@gmail.com>
2023-05-22 07:05:25 -0400

    added custom ui

--- FunctionFile.js
+++ FunctionFile.js
@@ -5,6 +5,8 @@
     };
 })();
 
+
+
 var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
